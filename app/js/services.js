43e5fc0abe707c5f7a3319345f1fae11b464bbd3
Joris Ooms <info@jorisooms.be>
2013-11-28 00:08:36 +0100

    add emailapiservice

--- app/js/services.js
+++ app/js/services.js
@@ -1,1 +1,15 @@
 'use strict';
+
+angular.module('SecretSantaApp.services', [])
+  .factory('emailAPIService', function($http) {
+    var emailAPIService = {};
+
+    emailAPIService.postEmails = function(data) {
+      $http.post('/handleEmails', data).
+        success(function(response) {
+          console.log(response);
+        });
+    };
+
+    return emailAPIService;
+  });
