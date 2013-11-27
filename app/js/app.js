ee1e4004d9e6e4cc0c589a5089280303d4f85f31
Joris Ooms <info@jorisooms.be>
2013-11-28 00:07:09 +0100

    add underscore dependency

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,9 @@
 'use strict';
 
 angular.module('SecretSantaApp', [
+  'underscore',
   'SecretSantaApp.controllers',
-  'SecretSantaApp.directives'
+  'SecretSantaApp.directives',
+  'SecretSantaApp.services'
   ]
 );
