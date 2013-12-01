99fa4e1c911ebd6ade0d41b7ef0771d2f68ccccb
Joris Ooms <info@jorisooms.be>
2013-12-01 16:30:55 +0100

    add slider configuration to eventctrl

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,14 +2,26 @@
 
 angular.module('SecretSantaApp.controllers', [])
   .controller('eventController', function($scope, emailAPIService, _) {
+    $('.slider').slider()
+
     $scope.event = {
       title: '',
       cashAmount: 15,
       cashCurrency: 'eur',
+      date: new Date("2013-12-25T00:00:00.000Z"),
       message: ''
     }
     $scope.people = [];
     $scope.newPerson = {};
+
+    $scope.slider = {
+      options: {
+        orientation: 'horizontal',
+        min: '0',
+        max: '100',
+        range: 'min'
+      }
+    }
 
     $scope.addPerson = function(person) {
       $scope.people.push(person);
