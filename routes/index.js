2bb2b8df49163ccaf8eebf6c4e8a48cf08071db0
Joris Ooms <info@jorisooms.be>
2013-12-03 03:28:20 +0100

    handle emails has been moved to app.js

--- routes/index.js
+++ routes/index.js
@@ -11,21 +11,3 @@ exports.index = function(env, ga) {
     res.render('index', { title: 'A memo from Santa!', env: env, ga: ga });
   }
 };
-
-exports.handleEmails = function(req, res) {
-  var postData = req.body,
-      mailData = {}, secretSanta;
-  console.log(postData);
-  mailData.event = postData.event;
-
-  secretSanta = (function() {
-    var len = postData.people.length, i;
-    for(i = 0; i < len; i++) {
-      _.sample(postData.people)
-    }
-  })();
-
-  console.log(mailData);
-  res.json(mailData);
-}
-
